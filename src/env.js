7ad24c7bc51265a0e35000a78fcf1b62a3caa810
principess01 <aurelijadarzinskaite@gmail.com>
2025-07-22 21:44:47 +0300

    env file update

--- src/env.js
+++ src/env.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+
 const argList = process.argv.slice(2);
 const args = {};
 
@@ -9,6 +11,12 @@ for (const str of argList) {
     }
 }
 
-console.log(args);
+dotenv.config({
+    path: 'src/.env.' + args.env,
+});
 
-export const PORT = 5517;
+export const PORT = +process.env.PORT ?? 5517;
+export const TITLE = process.env.TITLE ?? 'Project title';
+export const DB_DATABASE = process.env.DB_DATABASE ?? 'test_db';
+export const DB_USER = process.env.DB_USER ?? 'test_user';
+export const DB_PASSWORD = process.env.DB_PASSWORD ?? 'test_password';
